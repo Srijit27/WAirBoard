8e7d80a6c5ab2443b14d8dbb1460ad1c3e1b18eb
Srijit Roy <rsrijit436@gmail.com>
2026-01-22 21:07:31 +0530

    Structure setup

---
+++ weather-aqi-dashboard/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/vite.svg' /*changes needed*/
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Weather & AQI Dashboard</h1>
+      <p>React + Vite project</p>
+    </div>
+  );
+}
+
+export default App;
