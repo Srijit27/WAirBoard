ee7ec23bbeca50247a4ae265a764f16c5b47cf32
Srijit Roy <rsrijit436@gmail.com>
2026-03-10 00:14:45 +0530

    minor changes

--- weather-aqi-dashboard/src/services/WeatherAPI.js
+++ weather-aqi-dashboard/src/services/WeatherAPI.js
@@ -2,20 +2,16 @@ const API_KEY=import.meta.env.VITE_WEATHER_API_KEY;
 const BASE_URL="https://api.openweathermap.org/data/2.5";
 
 export async function getCityCoordinates(city){
-  const response = await fetch(
+  const response=await fetch(
     `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
   );
-
   if(!response.ok){
     throw new Error("Failed to fetch city coordinates");
   }
-
   const data=await response.json();
-
   if(data.length===0){
-    throw new Error("Invalid city name..!!");
+    throw new Error("The city name is invalid..!!");
   }
-
   return{
     lat:data[0].lat,
     lon:data[0].lon,
@@ -23,13 +19,11 @@ export async function getCityCoordinates(city){
     country:data[0].country,
   };
 }
-
 export async function getWeatherData(lat,lon){
   const response=await fetch(
     `${BASE_URL}/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
   );
-
-  if (!response.ok){
+  if(!response.ok){
     throw new Error("Failed to fetch weather data");
   }
   return response.json();
