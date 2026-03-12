90e3c89ccd1d6a949cea826683c395a5e2e32e70
Srijit Roy <rsrijit436@gmail.com>
2026-03-12 22:35:01 +0530

    Changes in weather card

--- weather-aqi-dashboard/src/components/WeatherCard.jsx
+++ weather-aqi-dashboard/src/components/WeatherCard.jsx
@@ -3,10 +3,10 @@ function WeatherCard({city,weather})
   return(
     <div style={styles.card}>
       <h2>{city}</h2>
-      <p>🌡Temperature:{weather.main.temp}°C</p>
-      <p>💧Humidity:{weather.main.humidity}%</p>
-      <p>🌬Wind Speed:{weather.wind.speed}m/s</p>
-      <p>☁Condition:{weather.weather[0].description}</p>
+      <p> 🌡 Temperature : {weather.main.temp}°C</p>
+      <p> 💧 Humidity : {weather.main.humidity}%</p>
+      <p> 🌬 Wind Speed : {weather.wind.speed}m/s</p>
+      <p> ☁ Condition : {weather.weather[0].description}</p>
     </div>
   );
 }
